87a3156980dc64531d1a4f20d835335da6e9817a
maxicozy <max@suedien.de>
2020-07-02 10:27:49 +0200

    added stuff i guess

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,22 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  props: {
+    test: {
+      type: String,
+    }
+  },
+  sockets: {
+        connect: function () {
+            console.log('socket connected')
+        },
+        nachrichtenname: function (data) {
+            console.log('this method was fired by the socket server. eg: io.emit("nachrichtenname"', data)
+        }
+  } 
+  //hier folgen ggf. noch methods, mounted() usw.
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import store from './store'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://3.121.41.235:3000',
+  vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+  },
+  options: { path: "/my-app/" } //Optional options
+}))
 
 Vue.config.productionTip = false
 
